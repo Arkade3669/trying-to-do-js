6d48d24f804b7e38dd4272b7ec7597c353857d01
Arkade3669 <112306670+Arkade3669@users.noreply.github.com>
2023-07-05 21:55:51 +0530

    uh

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("hello there")
+const fruits = ["banana", "orange", "mango"]
+document.getElementById("uwu").innerHTML - fruits.toString();
+document,write("hello");
+//it does not work, i think node is dead
